7e559806d01a15c683db360426aef7bd0010c446
papanda1234 <papanda1234@gmail.com>
2025-07-14 13:38:25 +0900

    Add files via upload
    fix bugs.

--- fjdispatchlite/test/test_sharednotify.cpp
+++ fjdispatchlite/test/test_sharednotify.cpp
@@ -5,12 +5,12 @@
 
 class Notifier : public FJSharedMem {
 public:
-    Notifier(const std::string& name) : FJSharedMem(name, 0) {}
+    Notifier(const std::string& name, const char *srcfunc) : FJSharedMem(name, 0, srcfunc) {}
     void update(FJSharedMem*, fjt_msg_t) override {}
 };
 
 int main() {
-    Notifier notifier("/fjtestmem");
+    Notifier notifier("/fjtestmem", __PRETTY_FUNCTION__);
     std::cout << "Sending notification...\n";
     notifier.notify(&notifier, 12345); // 適当なmsg
     std::cout << "Done.\n";
--- fjdispatchlite/test/test_sharedupdate.cpp
+++ fjdispatchlite/test/test_sharedupdate.cpp
@@ -5,24 +5,27 @@
 
 class Receiver : public FJSharedMem {
 public:
-    Receiver(const std::string& name) : FJSharedMem(name, 0) {
-        addListen(this, 12345); // 受信するmsgを登録
-		std::cerr << "listner" << this << std::endl;
+    Receiver(const std::string& name, const char *srcfunc) : FJSharedMem(name, 0, srcfunc, std::vector<fjt_msg_t>{12345, 12346, 12347} ) {
     }
 
     void update(FJSharedMem*obj, fjt_msg_t msg) override {
         std::cout << "Received " << obj << " ,notification: msg = " << msg << "\n";
+
+		int msg2 = 12345 + rand() % 2;
+        std::cout << "Send " << this << " ,notify: msg = " << msg2 << "\n";
+		notify(this, msg2);
+
     }
 };
 
 int main() {
-    Receiver receiver("/fjtestmem");
+    Receiver receiver("/fjtestmem", __PRETTY_FUNCTION__);
 
-    std::cout << "Waiting for notification 20sec...\n";
-	for (int i = 0; i < 20; i++) {
-        sleep(1);
+    std::cout << "Waiting for notification 15sec...\n";
+	for (int i = 0; i < 15; i++) {
+        sleep(15);
     }
-    std::cout << "end...\n";
+    std::cout << "Done.\n";
 
     return 0;
 }
